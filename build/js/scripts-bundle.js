9f811298f561287022edcf97b3a63d406c887b49
Maciej Sawicki <>
2020-03-03 23:23:15 +0100

    automatic commit from webflow-prototyping-framework

--- build/js/scripts-bundle.js
+++ build/js/scripts-bundle.js
@@ -22,6 +22,259 @@
 
 // Utility functions specific to the app not to framework
 
+//modal generic action
+$.On('click', '[js-add-rhyme-button]', function() {
+  StateURL.setParam('modalContent', 'eol-modal');
+});
+
+StateURL.onChange('modalContent', function(value) {
+  $('[js-eol-modal]').isShownWhen(value === 'eol-modal');
+});
+
+$.On('click', '[js-start-button]', function() {
+  var firstId = $('[js-rhyme-list-item]').attr('id');
+  scrollToRhyme(firstId);
+});
+
+function scrollToRhyme(rhymeId) {
+  var rhymeToScrollTo = $('[id="'+rhymeId+'"]');
+  $(window).scrollTo(rhymeToScrollTo, 500);
+}
+
+function nextRhyme() {
+  var currentId = StateURL.get('rym');
+  var adjacent;
+  if (isNotEmpty(currentId)) {
+    adjacent = $('[id="'+currentId+'"]').next();
+  } else {
+    adjacent = $('[js-rhyme-list-item]').first();
+  }
+  var newId = adjacent.attr('id');
+
+  if (isNotEmpty(newId)) {
+    scrollToRhyme(newId);
+  }
+}
+
+function previousRhyme() {
+  var currentId = StateURL.get('rym');
+  var adjacent = $('[id="'+currentId+'"]').prev();
+  var newId = adjacent.attr('id');
+  if (isNotEmpty(newId)) {
+    scrollToRhyme(newId);
+  } else {
+    $(window).scrollTo(0, 900);
+  }
+}
+
+var oneRhymeHeight;
+function initDynamicUrlWithRhymesId() {
+  oneRhymeHeight = $('[js-rhyme-list-item]').first().innerHeight();
+
+  $(window).on('scroll.dynamicURL', function() {
+    var scrollTop = $(this).scrollTop() - oneRhymeHeight/2;
+    var indexOfRhyme = scrollTop / oneRhymeHeight;
+    if (indexOfRhyme >= 0) {
+      indexOfRhyme = Math.floor(indexOfRhyme);
+      idOfRhyme = $('[js-rhyme-list-item]').eq(indexOfRhyme).attr('id');
+      StateURL.set('rym', idOfRhyme, {doNotCreateHistoryState: true});
+    } else {
+      StateURL.remove('rym');
+    }
+  });
+}
+initDynamicUrlWithRhymesId();
+$(window).on('resize', function() {
+  $(window).off('scroll.dynamicURL');
+  initDynamicUrlWithRhymesId();
+});
+
+$(document).on('renderingListReady', function() {
+  var rhymeId = StateURL.get('rym');
+  if (isNotEmpty(rhymeId)) {
+    scrollToRhyme(rhymeId);
+  }
+});
+
+$.On('click', '[js-nav-next]', function() {
+  nextRhyme();
+});
+
+$.On('click', '[js-nav-prev]', function() {
+  previousRhyme();
+});
+
+$(document).keydown(function(e) {
+  console.log(e.key);
+  if (e.key === "ArrowRight") {
+    nextRhyme();
+  } else if (e.key === "ArrowLeft") {
+    previousRhyme();
+  }
+});
+
+//TODO!!!!!!!!!
+/*
+Sprzątnąć build
+Zrobić update aplikacji APK
+*/
+
+// StateURL.onParamChange('subpage', function(value) {
+// 	if (typeof value != 'undefined') {
+// 	    showOnlyElementsWithAttributeXMatchingY('js-subpage', value);
+// 	}
+
+// 	$('[js-main-scroll-area]').scrollTo(0);
+// });
+
+// $.On('click', '[js-go-to-subpage]', function() {
+// 	var targetSubpage = $(this).attr('js-go-to-subpage');
+// 	StateURL.set('subpage', targetSubpage);
+// });
+
+// State.setDefault('user-logged-in', 'false');
+// State.onChange('user-logged-in', function(value) {
+//   $('[js-show-when-logged-in]').isShownWhen(value === 'true');
+//   $('[js-show-when-not-logged-in]').isShownWhen(value === 'false');
+// });
+
+// $.On('click', '[js-action-log-in]', function() {
+//   window.location.href = "/fake-auth";
+// });
+
+// StateURL.onChange('loggedIn', function(value) {
+//   if (value === "true") {
+//     State.set('user-logged-in', 'true');
+//     StateURL.set('modalContent', 'finish-profile');
+//     StateURL.remove('loggedIn', {doNotCreateHistoryState: true});
+//   }
+// });
+
+// $.On('click', '[js-action-log-out]', function() {
+//   State.set('user-logged-in', 'false');
+// });
+
+// //SPA navigation tabs -- currently unused, kept just in case
+// StateURL.onParamChange('mainTab', function(value) {
+// 	if (typeof value != 'undefined') {
+// 		$('[main-tab-id]').not('[main-tab-id="'+value+'"]').removeClass('is-current');
+// 		$('[main-tab-id="'+value+'"]').addClass('is-current');
+
+// 	    $('[secondary-navbar-id]').not('[secondary-navbar-id="'+value+'"]').addClass('is-hidden');
+// 	    $('[secondary-navbar-id="'+value+'"]').removeClass('is-hidden');
+// 	}
+// });
+
+// $.On('click', '[main-tab-id]', function(event) {
+// 	var valueToSet = $(event.currentTarget).attr('main-tab-id');
+// 	StateURL.setParam('mainTab', valueToSet);
+
+// 	//clicking on tab should load the first subtab content
+// 	var relatedSecondaryNavbarId = $(event.currentTarget).attr('main-tab-id');
+// 	var firstSecondaryTab = $('[secondary-navbar-id="'+relatedSecondaryNavbarId+'"]').find('[secondary-tab-id]').first();
+// 	var subpageId = firstSecondaryTab.attr('secondary-tab-id');
+// 	StateURL.setParam('subpage', subpageId, {doNotCreateHistoryState: true});
+// 	StateURL.setParam('secondaryTab', subpageId, {doNotCreateHistoryState: true});
+// });
+
+// $.On('click', '[secondary-tab-id]', function(event) {
+// 	var valueToSet = $(event.currentTarget).attr('secondary-tab-id');
+// 	setTimeout(function() {
+// 		StateURL.setParam('secondaryTab', valueToSet, {doNotCreateHistoryState: true});
+// 	}, 1);
+// 	StateURL.setParam('subpage', valueToSet);
+// });
+
+// StateURL.onParamChange('secondaryTab', function(value) {
+// 	$('[secondary-tab-id]').not('[secondary-tab-id="'+value+'"]').removeClass('is-current');
+// 	$('[secondary-tab-id="'+value+'"]').addClass('is-current');
+// });
+
+StateURL.onParamChange('subpage', function(value) {
+  if (typeof gtag !== 'undefined') {
+    gtag('event', 'Page view', {
+      'event_category': 'Navigation',
+      'event_label': value,
+    });
+
+    // if (value === "thank-you") {
+    //   gtag('event', 'Reached thank you page', {
+    //     'event_category': 'Navigation',
+    //   });
+    // }
+
+    //TODO add changes to tracked pagepath for Single Page App
+    //https://developers.google.com/analytics/devguides/collection/gtagjs/single-page-applications
+  }
+
+  // if (typeof fbq !== 'undefined') {
+
+  //   if (value === "thank-you") {
+  //     fbq('track', 'Thank you');
+  //   }
+
+  // }
+});
+
+// StateURL.onParamChange('modalContent', function(value) {
+//   if (typeof gtag !== 'undefined') {
+//     if (value === 'product-details') {
+//       gtag('event', 'Opened modal with energy product details', {
+//         'event_category': 'Navigation',
+//       });
+//     }
+//   }
+// });
+
+$.On('click', '[js-validate-demo-form]', function() {
+	var thisForm = $(this).closest('[js-demo-block-form]');
+	State.validateElementChildren(thisForm, {
+		onSuccess: function() {
+			FlashingNotifications.showAndHideNotification('success', 'All good!', 2000);
+		},
+		onError: function() {
+			var firstError = thisForm.find('[has-error]').first();
+			$('body').scrollTo(firstError, 1000, {offset: -100}); //you have full control of what happens after error
+		}
+	});
+});
+
+State.setValidationRules('demo-checkbox', function(value) {
+	if (value !== "true") {
+		return 'required';
+	} else {
+		return 'good';
+	}
+});
+
+State.setValidationRules('demo-checkbox-2', function(value) {
+	if (value !== "true") {
+		return 'required';
+	} else {
+		return 'good';
+	}
+});
+
+State.setValidationRules('demo-input-number', function(value) {
+	if (value === "") {
+		return 'required';
+	} else if (isNotNumeric(value)) {
+		return 'not-a-number';
+	} else {
+		return 'good';
+	}
+});
+
+State.setValidationRules('demo-textarea', function(value) {
+	if (value === "") {
+		return 'required';
+	} else if (value.length < 10) {
+		return 'too-short';
+	} else {
+		return 'good';
+	}
+});
+
 //Rendering all HTML list for server rendered HTML in Webflow
 //Not a part of any build, just use this in console and then extract HTML
 function renderRhymesList() {
@@ -5429,258 +5682,5 @@ $(document).on('preloadingComplete', function() {
   renderRhymesList();
 });
 
-
-//modal generic action
-$.On('click', '[js-add-rhyme-button]', function() {
-  StateURL.setParam('modalContent', 'eol-modal');
-});
-
-StateURL.onChange('modalContent', function(value) {
-  $('[js-eol-modal]').isShownWhen(value === 'eol-modal');
-});
-
-$.On('click', '[js-start-button]', function() {
-  var firstId = $('[js-rhyme-list-item]').attr('id');
-  scrollToRhyme(firstId);
-});
-
-function scrollToRhyme(rhymeId) {
-  var rhymeToScrollTo = $('[id="'+rhymeId+'"]');
-  $(window).scrollTo(rhymeToScrollTo, 500);
-}
-
-function nextRhyme() {
-  var currentId = StateURL.get('rym');
-  var adjacent;
-  if (isNotEmpty(currentId)) {
-    adjacent = $('[id="'+currentId+'"]').next();
-  } else {
-    adjacent = $('[js-rhyme-list-item]').first();
-  }
-  var newId = adjacent.attr('id');
-
-  if (isNotEmpty(newId)) {
-    scrollToRhyme(newId);
-  }
-}
-
-function previousRhyme() {
-  var currentId = StateURL.get('rym');
-  var adjacent = $('[id="'+currentId+'"]').prev();
-  var newId = adjacent.attr('id');
-  if (isNotEmpty(newId)) {
-    scrollToRhyme(newId);
-  } else {
-    $(window).scrollTo(0, 900);
-  }
-}
-
-var oneRhymeHeight;
-function initDynamicUrlWithRhymesId() {
-  oneRhymeHeight = $('[js-rhyme-list-item]').first().innerHeight();
-
-  $(window).on('scroll.dynamicURL', function() {
-    var scrollTop = $(this).scrollTop() - oneRhymeHeight/2;
-    var indexOfRhyme = scrollTop / oneRhymeHeight;
-    if (indexOfRhyme >= 0) {
-      indexOfRhyme = Math.floor(indexOfRhyme);
-      idOfRhyme = $('[js-rhyme-list-item]').eq(indexOfRhyme).attr('id');
-      StateURL.set('rym', idOfRhyme, {doNotCreateHistoryState: true});
-    } else {
-      StateURL.remove('rym');
-    }
-  });
-}
-initDynamicUrlWithRhymesId();
-$(window).on('resize', function() {
-  $(window).off('scroll.dynamicURL');
-  initDynamicUrlWithRhymesId();
-});
-
-$(document).on('renderingListReady', function() {
-  var rhymeId = StateURL.get('rym');
-  if (isNotEmpty(rhymeId)) {
-    scrollToRhyme(rhymeId);
-  }
-});
-
-$.On('click', '[js-nav-next]', function() {
-  nextRhyme();
-});
-
-$.On('click', '[js-nav-prev]', function() {
-  previousRhyme();
-});
-
-$(document).keydown(function(e) {
-  console.log(e.key);
-  if (e.key === "ArrowRight") {
-    nextRhyme();
-  } else if (e.key === "ArrowLeft") {
-    previousRhyme();
-  }
-});
-
-//TODO!!!!!!!!!
-/*
-Sprzątnąć build
-Zrobić update aplikacji APK
-*/
-
-// StateURL.onParamChange('subpage', function(value) {
-// 	if (typeof value != 'undefined') {
-// 	    showOnlyElementsWithAttributeXMatchingY('js-subpage', value);
-// 	}
-
-// 	$('[js-main-scroll-area]').scrollTo(0);
-// });
-
-// $.On('click', '[js-go-to-subpage]', function() {
-// 	var targetSubpage = $(this).attr('js-go-to-subpage');
-// 	StateURL.set('subpage', targetSubpage);
-// });
-
-// State.setDefault('user-logged-in', 'false');
-// State.onChange('user-logged-in', function(value) {
-//   $('[js-show-when-logged-in]').isShownWhen(value === 'true');
-//   $('[js-show-when-not-logged-in]').isShownWhen(value === 'false');
-// });
-
-// $.On('click', '[js-action-log-in]', function() {
-//   window.location.href = "/fake-auth";
-// });
-
-// StateURL.onChange('loggedIn', function(value) {
-//   if (value === "true") {
-//     State.set('user-logged-in', 'true');
-//     StateURL.set('modalContent', 'finish-profile');
-//     StateURL.remove('loggedIn', {doNotCreateHistoryState: true});
-//   }
-// });
-
-// $.On('click', '[js-action-log-out]', function() {
-//   State.set('user-logged-in', 'false');
-// });
-
-// //SPA navigation tabs -- currently unused, kept just in case
-// StateURL.onParamChange('mainTab', function(value) {
-// 	if (typeof value != 'undefined') {
-// 		$('[main-tab-id]').not('[main-tab-id="'+value+'"]').removeClass('is-current');
-// 		$('[main-tab-id="'+value+'"]').addClass('is-current');
-
-// 	    $('[secondary-navbar-id]').not('[secondary-navbar-id="'+value+'"]').addClass('is-hidden');
-// 	    $('[secondary-navbar-id="'+value+'"]').removeClass('is-hidden');
-// 	}
-// });
-
-// $.On('click', '[main-tab-id]', function(event) {
-// 	var valueToSet = $(event.currentTarget).attr('main-tab-id');
-// 	StateURL.setParam('mainTab', valueToSet);
-
-// 	//clicking on tab should load the first subtab content
-// 	var relatedSecondaryNavbarId = $(event.currentTarget).attr('main-tab-id');
-// 	var firstSecondaryTab = $('[secondary-navbar-id="'+relatedSecondaryNavbarId+'"]').find('[secondary-tab-id]').first();
-// 	var subpageId = firstSecondaryTab.attr('secondary-tab-id');
-// 	StateURL.setParam('subpage', subpageId, {doNotCreateHistoryState: true});
-// 	StateURL.setParam('secondaryTab', subpageId, {doNotCreateHistoryState: true});
-// });
-
-// $.On('click', '[secondary-tab-id]', function(event) {
-// 	var valueToSet = $(event.currentTarget).attr('secondary-tab-id');
-// 	setTimeout(function() {
-// 		StateURL.setParam('secondaryTab', valueToSet, {doNotCreateHistoryState: true});
-// 	}, 1);
-// 	StateURL.setParam('subpage', valueToSet);
-// });
-
-// StateURL.onParamChange('secondaryTab', function(value) {
-// 	$('[secondary-tab-id]').not('[secondary-tab-id="'+value+'"]').removeClass('is-current');
-// 	$('[secondary-tab-id="'+value+'"]').addClass('is-current');
-// });
-
-StateURL.onParamChange('subpage', function(value) {
-  if (typeof gtag !== 'undefined') {
-    gtag('event', 'Page view', {
-      'event_category': 'Navigation',
-      'event_label': value,
-    });
-
-    // if (value === "thank-you") {
-    //   gtag('event', 'Reached thank you page', {
-    //     'event_category': 'Navigation',
-    //   });
-    // }
-
-    //TODO add changes to tracked pagepath for Single Page App
-    //https://developers.google.com/analytics/devguides/collection/gtagjs/single-page-applications
-  }
-
-  // if (typeof fbq !== 'undefined') {
-
-  //   if (value === "thank-you") {
-  //     fbq('track', 'Thank you');
-  //   }
-
-  // }
-});
-
-// StateURL.onParamChange('modalContent', function(value) {
-//   if (typeof gtag !== 'undefined') {
-//     if (value === 'product-details') {
-//       gtag('event', 'Opened modal with energy product details', {
-//         'event_category': 'Navigation',
-//       });
-//     }
-//   }
-// });
-
-$.On('click', '[js-validate-demo-form]', function() {
-	var thisForm = $(this).closest('[js-demo-block-form]');
-	State.validateElementChildren(thisForm, {
-		onSuccess: function() {
-			FlashingNotifications.showAndHideNotification('success', 'All good!', 2000);
-		},
-		onError: function() {
-			var firstError = thisForm.find('[has-error]').first();
-			$('body').scrollTo(firstError, 1000, {offset: -100}); //you have full control of what happens after error
-		}
-	});
-});
-
-State.setValidationRules('demo-checkbox', function(value) {
-	if (value !== "true") {
-		return 'required';
-	} else {
-		return 'good';
-	}
-});
-
-State.setValidationRules('demo-checkbox-2', function(value) {
-	if (value !== "true") {
-		return 'required';
-	} else {
-		return 'good';
-	}
-});
-
-State.setValidationRules('demo-input-number', function(value) {
-	if (value === "") {
-		return 'required';
-	} else if (isNotNumeric(value)) {
-		return 'not-a-number';
-	} else {
-		return 'good';
-	}
-});
-
-State.setValidationRules('demo-textarea', function(value) {
-	if (value === "") {
-		return 'required';
-	} else if (value.length < 10) {
-		return 'too-short';
-	} else {
-		return 'good';
-	}
-});
 
 //# sourceMappingURL=scripts-bundle.js.map
