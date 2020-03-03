8c08498823bbe89b696db6b053006f1444f0177d
Maciej Sawicki <>
2020-03-03 21:47:42 +0100

    automatic commit from webflow-prototyping-framework

--- build/js/scripts-bundle.js
+++ build/js/scripts-bundle.js
@@ -5516,11 +5516,7 @@ $(document).on('preloadingComplete', function() {
 
 //TODO!!!!!!!!!
 /*
-Top 200 zamiast 500
 Sprzątnąć build
-Przepisać Kilkanaście na Webflow
-Renderować dynamicznie resztę dopiero jak ktoś dojedzie
-Poprawić FB share button na customowy z linkiem do sharer?
 Zrobić update aplikacji APK
 */
 
